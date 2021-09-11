7173821a6b6e14227ec01a1ff5529c32021ff530
Vesurn <ikarostf2@gmail.com>
2021-09-11 13:52:14 +0300

    fixup! fixup! update Procfile

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ app.get('/', (req, res) => {
 })
 
 app.get('/index.js', (req, res) => {
-    res.sendFile('/index.js', {root: `${__dirname}/../`})
+    res.sendFile('client/index.js', {root: `${__dirname}/../`})
 })
 
 function logger(req, res, next) {
